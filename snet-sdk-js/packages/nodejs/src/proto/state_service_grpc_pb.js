ab9a6f2a5df0a531c62617a1826dbf564dbe05e6
Brijesh Akhiya <brijeshakhiya@gmail.com>
2024-01-04 20:47:18 +0530

    two file conversin

--- snet-sdk-js/packages/nodejs/src/proto/state_service_grpc_pb.js
+++ snet-sdk-js/packages/nodejs/src/proto/state_service_grpc_pb.js
@@ -1,53 +1,56 @@
-// GENERATED CODE -- DO NOT EDIT!
-/* eslint-disable */
-'use strict';
-import grpc from '@grpc/grpc-js';
-var state_service_pb = require('./state_service_pb.js');
+// state_service_pb.js needs to be imported as an ESM module for this to work
 
-function serialize_escrow_ChannelStateReply(arg) {
-  if (!(arg instanceof state_service_pb.ChannelStateReply)) {
+import grpc from '@grpc/grpc-js';
+import {
+  ChannelStateReply,
+  ChannelStateRequest,
+  FreeCallStateReply,
+  FreeCallStateRequest,
+} from './state_service_pb.js';
+
+export function serialize_escrow_ChannelStateReply(arg) {
+  if (!(arg instanceof ChannelStateReply)) {
     throw new Error('Expected argument of type escrow.ChannelStateReply');
   }
   return Buffer.from(arg.serializeBinary());
 }
 
-function deserialize_escrow_ChannelStateReply(buffer_arg) {
-  return state_service_pb.ChannelStateReply.deserializeBinary(new Uint8Array(buffer_arg));
+export function deserialize_escrow_ChannelStateReply(buffer_arg) {
+  return ChannelStateReply.deserializeBinary(new Uint8Array(buffer_arg));
 }
 
-function serialize_escrow_ChannelStateRequest(arg) {
-  if (!(arg instanceof state_service_pb.ChannelStateRequest)) {
+export function serialize_escrow_ChannelStateRequest(arg) {
+  if (!(arg instanceof ChannelStateRequest)) {
     throw new Error('Expected argument of type escrow.ChannelStateRequest');
   }
   return Buffer.from(arg.serializeBinary());
 }
 
-function deserialize_escrow_ChannelStateRequest(buffer_arg) {
-  return state_service_pb.ChannelStateRequest.deserializeBinary(new Uint8Array(buffer_arg));
+export function deserialize_escrow_ChannelStateRequest(buffer_arg) {
+  return ChannelStateRequest.deserializeBinary(new Uint8Array(buffer_arg));
 }
 
-function serialize_escrow_FreeCallStateReply(arg) {
-  if (!(arg instanceof state_service_pb.FreeCallStateReply)) {
+export function serialize_escrow_FreeCallStateReply(arg) {
+  if (!(arg instanceof FreeCallStateReply)) {
     throw new Error('Expected argument of type escrow.FreeCallStateReply');
   }
   return Buffer.from(arg.serializeBinary());
 }
 
-function deserialize_escrow_FreeCallStateReply(buffer_arg) {
-  return state_service_pb.FreeCallStateReply.deserializeBinary(new Uint8Array(buffer_arg));
+export function deserialize_escrow_FreeCallStateReply(buffer_arg) {
+  return FreeCallStateReply.deserializeBinary(new Uint8Array(buffer_arg));
 }
 
-function serialize_escrow_FreeCallStateRequest(arg) {
-  if (!(arg instanceof state_service_pb.FreeCallStateRequest)) {
+export function serialize_escrow_FreeCallStateRequest(arg) {
+  if (!(arg instanceof FreeCallStateRequest)) {
     throw new Error('Expected argument of type escrow.FreeCallStateRequest');
   }
   return Buffer.from(arg.serializeBinary());
 }
 
-function deserialize_escrow_FreeCallStateRequest(buffer_arg) {
-  return state_service_pb.FreeCallStateRequest.deserializeBinary(new Uint8Array(buffer_arg));
+export function deserialize_escrow_FreeCallStateRequest(buffer_arg) {
+  return FreeCallStateRequest.deserializeBinary(new Uint8Array(buffer_arg));
 }
-
 
 // PaymentChannelStateService contains methods to get the MultiPartyEscrow
 // payment channel state.
@@ -57,13 +60,12 @@ function deserialize_escrow_FreeCallStateRequest(buffer_arg) {
 // These values may be or may be not padded by zeros, service supports both
 // options.
 var PaymentChannelStateServiceService = exports.PaymentChannelStateServiceService = {
-  // GetChannelState method returns a channel state by channel id.
-getChannelState: {
+  getChannelState: {
     path: '/escrow.PaymentChannelStateService/GetChannelState',
     requestStream: false,
     responseStream: false,
-    requestType: state_service_pb.ChannelStateRequest,
-    responseType: state_service_pb.ChannelStateReply,
+    requestType: ChannelStateRequest,
+    responseType: ChannelStateReply,
     requestSerialize: serialize_escrow_ChannelStateRequest,
     requestDeserialize: deserialize_escrow_ChannelStateRequest,
     responseSerialize: serialize_escrow_ChannelStateReply,
@@ -72,14 +74,15 @@ getChannelState: {
 };
 
 exports.PaymentChannelStateServiceClient = grpc.makeClientConstructor(PaymentChannelStateServiceService);
+
 // Used to determine free calls available for a given user.
 var FreeCallStateServiceService = exports.FreeCallStateServiceService = {
   getFreeCallsAvailable: {
     path: '/escrow.FreeCallStateService/GetFreeCallsAvailable',
     requestStream: false,
     responseStream: false,
-    requestType: state_service_pb.FreeCallStateRequest,
-    responseType: state_service_pb.FreeCallStateReply,
+    requestType: FreeCallStateRequest,
+    responseType: FreeCallStateReply,
     requestSerialize: serialize_escrow_FreeCallStateRequest,
     requestDeserialize: deserialize_escrow_FreeCallStateRequest,
     responseSerialize: serialize_escrow_FreeCallStateReply,
